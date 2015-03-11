597b775c2c523592b118289d7ba50a1968ef4e95
busido90 <nagatai0120@gmail.com>
2015-03-11 10:34:11 +0800

    プルプル

--- datadump.sql
+++ datadump.sql
@@ -1,125 +1,120 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9.2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.6.23, for Linux (x86_64)
 --
--- ホスト: localhost
--- 生成時間: 2015 年 3 月 09 日 11:45
--- サーバのバージョン: 5.5.9
--- PHP のバージョン: 5.3.6
+-- Host: localhost    Database: gourrepo
+-- ------------------------------------------------------
+-- Server version	5.6.23
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- データベース: `GourRepo`
---
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `movies`
+-- Table structure for table `movies`
 --
 
 DROP TABLE IF EXISTS `movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `movies` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `count` int(11) NOT NULL DEFAULT '0',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `count` int(11) NOT NULL,
   `description` varchar(1600) NOT NULL,
-  `youtube_url` varchar(255) DEFAULT NULL,
-  `thumbnails_url` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
   `created` datetime NOT NULL,
-  `del_flg` tinyint(1) NOT NULL DEFAULT '0',
+  `del_flg` tinyint(1) NOT NULL,
   `created_user_id` int(11) NOT NULL,
   `modified` datetime NOT NULL,
   `modified_user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルのデータをダンプしています `movies`
+-- Dumping data for table `movies`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `movies` WRITE;
+/*!40000 ALTER TABLE `movies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movies` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- テーブルの構造 `restaurants`
+-- Table structure for table `restaurants`
 --
 
 DROP TABLE IF EXISTS `restaurants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `restaurants` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `gournabi_id` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `tel` varchar(255) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `latitude` int(11) DEFAULT NULL,
-  `longitude` int(11) DEFAULT NULL,
-  `category` varchar(255) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `url_mobile` varchar(255) DEFAULT NULL,
-  `opentime` varchar(255) DEFAULT NULL,
-  `holliday` varchar(255) DEFAULT NULL,
-  `access_line` varchar(255) DEFAULT NULL,
-  `access_station` varchar(255) DEFAULT NULL,
-  `access_station_exit` varchar(255) DEFAULT NULL,
-  `access_walk` varchar(255) DEFAULT NULL,
-  `access_note` varchar(255) DEFAULT NULL,
-  `parking_lots` int(11) DEFAULT NULL,
-  `pr` varchar(1000) DEFAULT NULL,
-  `code_areacode` varchar(255) DEFAULT NULL,
-  `code_areaname` varchar(255) DEFAULT NULL,
-  `code_prefcode` varchar(255) DEFAULT NULL,
-  `code_prefname` varchar(255) DEFAULT NULL,
-  `budget` int(11) DEFAULT NULL,
-  `party` int(11) DEFAULT NULL,
-  `lunch` int(11) DEFAULT NULL,
-  `credit_card` varchar(255) DEFAULT NULL,
-  `equipment` varchar(2000) DEFAULT NULL,
-  `del_flg` tinyint(1) NOT NULL DEFAULT '0',
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `tel` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `latitude` int(11) NOT NULL,
+  `longitude` int(11) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `url_mobile` varchar(255) NOT NULL,
+  `opentime` varchar(255) NOT NULL,
+  `holliday` varchar(255) NOT NULL,
+  `access_line` varchar(255) NOT NULL,
+  `access_station` varchar(255) NOT NULL,
+  `access_station_exit` varchar(255) NOT NULL,
+  `access_walk` int(11) NOT NULL,
+  `access_note` varchar(255) NOT NULL,
+  `parking_lots` int(11) NOT NULL,
+  `pr` varchar(1000) NOT NULL,
+  `code_areacode` varchar(255) NOT NULL,
+  `code_areaname` varchar(255) NOT NULL,
+  `code_prefcode` varchar(255) NOT NULL,
+  `code_prefname` varchar(255) NOT NULL,
+  `code_category_code_I` varchar(255) NOT NULL,
+  `code_category_code_I_order` int(11) NOT NULL,
+  `code_category_name_I` varchar(255) NOT NULL,
+  `code_category_name_I_order` int(11) NOT NULL,
+  `code_category_code_s` varchar(255) NOT NULL,
+  `code_category_code_s_order` int(11) NOT NULL,
+  `code_category_name_s` varchar(255) NOT NULL,
+  `code_category_name_s_order` int(11) NOT NULL,
+  `budget` int(11) NOT NULL,
+  `party` int(11) NOT NULL,
+  `lunch` int(11) NOT NULL,
+  `credit_card` varchar(255) NOT NULL,
+  `equipment` varchar(255) NOT NULL,
+  `del_flg` tinyint(1) NOT NULL,
   `created` datetime NOT NULL,
   `created_user_id` int(11) NOT NULL,
   `modified` datetime NOT NULL,
-  `modified_user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
+  `modified_user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルのデータをダンプしています `restaurants`
+-- Dumping data for table `restaurants`
 --
 
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `tags`
---
-
-DROP TABLE IF EXISTS `tags`;
-CREATE TABLE `tags` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `created_user_id` int(11) NOT NULL,
-  `modified` datetime NOT NULL,
-  `modified_user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+LOCK TABLES `restaurants` WRITE;
+/*!40000 ALTER TABLE `restaurants` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restaurants` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- テーブルのデータをダンプしています `tags`
---
-
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `tag_relations`
+-- Table structure for table `tag_relations`
 --
 
 DROP TABLE IF EXISTS `tag_relations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tag_relations` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tag_id` int(11) NOT NULL,
   `movie_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
@@ -127,46 +122,54 @@ CREATE TABLE `tag_relations` (
   `modified` datetime NOT NULL,
   `modified_user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルのデータをダンプしています `tag_relations`
+-- Dumping data for table `tag_relations`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `tag_relations` WRITE;
+/*!40000 ALTER TABLE `tag_relations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag_relations` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- テーブルの構造 `users`
+-- Table structure for table `tags`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `del_flg` tinyint(1) NOT NULL DEFAULT '0',
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `created_user_id` int(11) NOT NULL,
   `modified` datetime NOT NULL,
   `modified_user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルのデータをダンプしています `users`
+-- Dumping data for table `tags`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- テーブルの構造 `user_favorite_movie_lists`
+-- Table structure for table `user_favorite_movie_lists`
 --
 
 DROP TABLE IF EXISTS `user_favorite_movie_lists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_favorite_movie_lists` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `movie_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
@@ -174,22 +177,27 @@ CREATE TABLE `user_favorite_movie_lists` (
   `modified` datetime NOT NULL,
   `modified_user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルのデータをダンプしています `user_favorite_movie_lists`
+-- Dumping data for table `user_favorite_movie_lists`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `user_favorite_movie_lists` WRITE;
+/*!40000 ALTER TABLE `user_favorite_movie_lists` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_favorite_movie_lists` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- テーブルの構造 `user_profiles`
+-- Table structure for table `user_profiles`
 --
 
 DROP TABLE IF EXISTS `user_profiles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_profiles` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `like_food` varchar(255) NOT NULL,
@@ -199,28 +207,33 @@ CREATE TABLE `user_profiles` (
   `living_area` varchar(255) NOT NULL,
   `introduction` varchar(255) NOT NULL,
   `avatar_file_name` varchar(255) NOT NULL,
-  `del_flg` tinyint(1) NOT NULL DEFAULT '0',
+  `del_flg` tinyint(1) NOT NULL,
   `created` datetime NOT NULL,
   `created_user_id` int(11) NOT NULL,
   `modified` datetime NOT NULL,
   `modified_user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルのデータをダンプしています `user_profiles`
+-- Dumping data for table `user_profiles`
 --
 
-
--- --------------------------------------------------------
+LOCK TABLES `user_profiles` WRITE;
+/*!40000 ALTER TABLE `user_profiles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_profiles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- テーブルの構造 `user_watch_movie_lists`
+-- Table structure for table `user_watch_movie_lists`
 --
 
 DROP TABLE IF EXISTS `user_watch_movie_lists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_watch_movie_lists` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `movie_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
@@ -228,8 +241,56 @@ CREATE TABLE `user_watch_movie_lists` (
   `modified` datetime NOT NULL,
   `modified_user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- テーブルのデータをダンプしています `user_watch_movie_lists`
+-- Dumping data for table `user_watch_movie_lists`
 --
+
+LOCK TABLES `user_watch_movie_lists` WRITE;
+/*!40000 ALTER TABLE `user_watch_movie_lists` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_watch_movie_lists` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `del_flg` tinyint(1) NOT NULL,
+  `created` datetime NOT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `modified` datetime NOT NULL,
+  `modified_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'nagayama90@gmail.com','nagatai3113','admin',0,'2015-03-08 20:32:06',0,'2015-03-08 20:32:06',0),(2,'nagatai0120@gmail.com','nagatai3113','admin',0,'2015-03-08 21:21:44',0,'2015-03-08 21:21:44',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-03-11  9:31:37
